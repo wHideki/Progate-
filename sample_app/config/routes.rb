fc6be7188dd1c84e3bf1bc37464ff36f04b71c21
Watanabe <822w.hideki@gmail.com>
2020-04-02 17:47:19 +0900

    ユーザー登録機能の追加

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
