66f8be80e7f238ada833f38be9a1a9d3cd474732
Watanabe <822w.hideki@gmail.com>
2020-04-04 00:48:13 +0900

    プロフィール画像設定機能の追加

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_062742) do
+ActiveRecord::Schema.define(version: 2020_04_03_142316) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_062742) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
