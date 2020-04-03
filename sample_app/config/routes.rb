c69885af3e5ff710b401f2be52781204c49b87e3
Watanabe <822w.hideki@gmail.com>
2020-04-03 15:22:29 +0900

    ユーザー情報の編集機能の追加

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
   get "signup" => "users#new"
   get "users/index" => "users#index"
