aecb7a5f4bec800c03ac38d116165f0557c98af0
Watanabe <822w.hideki@gmail.com>
2020-04-14 12:45:47 +0900

    ログイン、ログアウト機能を追加しました

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -15,9 +15,11 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name], 
       email: params[:email],
-      image_name: "default_user.jpg"
+      image_name: "default_user.jpg",
+      password: params[:password]
       )
     if @user.save
+      session[:user_id]= @user.id
       flash[:notice]= "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -45,5 +47,28 @@ class UsersController < ApplicationController
     else
       render("users/edit")
     end
+  end
+
+  def login_form
+  end
+  
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 end
